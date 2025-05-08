7f755bd7c2d9677a1bef8744ccf620abc30507b6
Olga Pinheiro <olga.pinheiro@jusbrasil.com.br>
2025-05-07 23:23:17 -0300

    feat: add home

---
+++ portfolio/src/app/page.tsx
@@ -0,0 +1,25 @@
+export default function Home() {
+  return (
+    <div className="static grid min-h-full p-0 gap-0 font-[family-name:var(--font-geist-sans)]">
+      <main className="fixed min-h-full w-full">
+        {/* <!-- Background image --> */}
+        <div
+          className="h-screen bg-center bg-cover bg-no-repeat bg-[url(/exteriores/2-thewestinportodegalinhas.webp)]"></div>
+        {/* <!-- Background image --> */}
+      </main>
+      <footer className="bg-white fixed inset-x-0 bottom-0 flex gap-[24px] flex-wrap items-center justify-center py-4">
+        <div
+          className="flex items-center gap-2 font-bold text-2xl"
+        >
+          BIECO GARCIA
+        </div>
+        <div
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4 text-sm"
+        >
+          PORTFÓLIO DE FOTOGRAFIA<br />
+          PARA O SETOR DE HOTELARIA
+        </div>
+      </footer>
+    </div>
+  );
+}
