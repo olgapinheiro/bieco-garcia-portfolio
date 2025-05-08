48516a09d2d456a2bd690ee027da681bf547e9c1
Olga Pinheiro <olga.pinheiro@jusbrasil.com.br>
2025-05-08 15:03:49 -0300

    feat: use Gallery and `getImagesAction` to implement portfolio sections pages

--- src/app/portfolio/foods-and-drinks/page.tsx
+++ src/app/portfolio/foods-and-drinks/page.tsx
@@ -1,5 +1,18 @@
+import Gallery from "@/components/Gallery";
+import { getImagesAction } from "@/utils/getImagesAction";
+import { Suspense } from "react";
+
 const FoodsAndDrinksPage = () => {
-  return <p>Not Implemented</p>
+  const imagesArrayPromise = getImagesAction('foods-and-drinks');
+
+  return (
+    <main className="grid grid-cols-1 p-8 xs:p-16 gap-4 sm:gap-x-16 sm:gap-y-4 md:gap-x-32">
+      <div className="text-xl font-medium text-center">Foods & Drinks</div>
+      <Suspense>
+        <Gallery imagesPromise={imagesArrayPromise} directoryName="foods-and-drinks"/>
+      </Suspense>
+    </main>
+  )
 }
 
 export default FoodsAndDrinksPage
--- src/app/portfolio/outsides/page.tsx
+++ src/app/portfolio/outsides/page.tsx
@@ -1,5 +1,18 @@
-const OutsidesPage = () => {
-  return <p>Not Implemented</p>
+import Gallery from "@/components/Gallery"
+import { getImagesAction } from "@/utils/getImagesAction"
+import { Suspense } from "react";
+
+const OutsidesPage = async () => {
+  const imagesArrayPromise = getImagesAction('outsides');
+
+  return (
+    <main className="grid grid-cols-1 p-8 xs:p-16 gap-4 sm:gap-x-16 sm:gap-y-4 md:gap-x-32">
+      <div className="text-xl font-medium text-center">Outsides</div>
+      <Suspense>
+        <Gallery imagesPromise={imagesArrayPromise} directoryName="outsides"/>
+      </Suspense>
+    </main>
+  )
 }
 
 export default OutsidesPage
