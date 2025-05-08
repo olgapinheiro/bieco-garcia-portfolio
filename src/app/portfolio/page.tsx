c8da7b6e8dd2e631b7ff87a2260c2e4053f71dae
Olga Pinheiro <olga.pinheiro@jusbrasil.com.br>
2025-05-08 10:09:13 -0300

    feat: implement portfolio index page

---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,81 @@
+import Image from "next/image";
+import outsides from "../../../public/outsides/8-tabaobi.jpg"
+import insides from "../../../public/insides/11-filhadaluapipa.webp"
+import foodsAndDrinks from "../../../public/foods-and-drinks/2-refugiodoriobonito.webp"
+import lifestyle from "../../../public/lifestyle/4_filhadaluapipa.webp"
+// import aerials from "../../../public/outsides/8-tabaobi.jpg"
+import destinations from "../../../public/destinations/1-reveilloncarneiros.webp"
+import Link from "next/link";
+
+const IMAGES = [
+  {
+    src: outsides,
+    label: 'outsides',
+    key: 'outsides',
+    position: 'center',
+  },
+  {
+    src: insides,
+    label: 'insides',
+    key: 'insides',
+    position: 'top',
+  },
+  {
+    src: foodsAndDrinks,
+    label: 'foods & drinks',
+    key: 'foods-and-drinks',
+    position: 'center',
+  },
+  {
+    src: lifestyle,
+    label: 'lifestyle',
+    key: 'lifestyle',
+    position: 'center',
+  },
+  // {
+  //   src: '/outsides/8-tabaobi.jpg',
+  //   label: 'aerials',
+  //   key: 'aerials',
+  //   position: 'center',
+  // },
+  {
+    src: destinations,
+    label: 'destinations',
+    key: 'destinations',
+    position: 'center',
+  }
+]
+
+export default function Home() {
+  return (
+    <div className="static grid min-h-full p-0 gap-0 font-[family-name:var(--font-geist-sans)]">
+      <header className=" outline bg-white flex items-center justify-center py-4">
+        <div
+          className="flex items-center gap-2 font-bold text-4xl"
+        >
+          BIECO GARCIA
+        </div>
+      </header>
+      <main className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 p-8 xs:p-16 gap-4 sm:gap-x-16 sm:gap-y-4 md:gap-x-32">
+        {IMAGES.map((image) => (
+          <div className="grid grid-cols-1 justify-items-center" key={`cover-${image.key}`}>
+            <Link className="relative w-full" href={`portfolio/${image.key}`}>
+              <Image
+                width="1138"
+                height="1138"
+                src={image.src}
+                alt=""
+                // sizes="(max-width: 768px) 100vw, 33vw"
+                className={`object-cover aspect-square`}
+                style={{
+                  objectPosition: `${image.position}`,
+                }}
+              />
+            </Link>
+            <div className="py-4">{ image.label }</div>
+          </div>
+        ))}
+      </main>
+    </div>
+  );
+}
