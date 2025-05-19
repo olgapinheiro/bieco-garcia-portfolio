25d96021445f43fcbe62901113b56859b820e4f5
Olga Pinheiro <olga.pinheiro@jusbrasil.com.br>
2025-05-19 08:50:45 -0300

    feat: improve modal style

--- src/components/ImageModal.tsx
+++ src/components/ImageModal.tsx
@@ -16,30 +16,27 @@ const ImageModal = ({
     <div>
       {modalOpen &&
         <div
-          className='z-10 fixed top-0 left-0 w-full h-full bg-white/95 flex justify-center items-center'
+          className='z-10 fixed top-0 left-0 w-full h-full bg-white/95 flex justify-center items-center p-8'
           role="dialog"
           aria-modal="true"
           onClick={handleModal}
         >
-          <div className='bg-white shadow-lg m-8'>
-            <Image
-              src={imageUrl}
-              width={500}
-              height={500}
-              alt=""
-              style={{ objectFit: 'contain', height: 'auto', width: '100%' }}
-            />
-            <div className='fixed rounded-full bottom-8 sm:bottom-auto sm:inset-y-8 right-4 sm:right-8 flex items-center px-4 pt-2'>
-              <button
-                type='button'
-                className='h-8 w-8 px-2 text-sm rounded-full bg-white/95 text-black cursor-pointer font-bold border border-gray-300/50'
-                onClick={handleModal}
-                aria-label="Close image"
-              >
-                x
-              </button>
-            </div>
-          </div>
+          <Image
+            src={imageUrl}
+            width={500}
+            height={500}
+            alt=""
+            className='h-full w-full'
+            style={{ objectFit: 'contain', width: '100%', maxHeight: 'vh' }}
+          />
+            <button
+              type='button'
+              className='fixed bottom-8 sm:bottom-auto sm:inset-y-8 right-4 sm:right-8 h-8 w-8 px-2 text-sm rounded-full bg-white/50 text-black cursor-pointer font-bold border border-gray-300/50'
+              onClick={handleModal}
+              aria-label="Close image"
+            >
+              x
+            </button>
         </div>
       }
 
