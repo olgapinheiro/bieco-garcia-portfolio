64206a961c651b005ec165ca5a343bab283b5b62
Olga Pinheiro <olga.pinheiro@jusbrasil.com.br>
2025-08-02 01:45:49 -0300

    feat: create desktop and mobile navs

---
+++ src/components/NavDesktop.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import { EmailIcon, InstagramIcon, WhatsappIcon } from "./icons";
+import SocialMediaIcons from "./SocialMediaIcons";
+
+
+export default function NavDesktop() {
+	return (
+		<div className="hidden md:flex gap-6 flex-wrap items-center justify-between">
+			<div className="hidden md:flex gap-6 flex-no-wrap items-center justify-between">
+				<Link
+					className=""
+					href="/about"
+				>
+					About
+				</Link>
+				<Link
+					className=""
+					href="/portfolio"
+				>
+					Work
+				</Link>
+			</div>
+
+			<SocialMediaIcons />
+		</div>
+	)
+}
