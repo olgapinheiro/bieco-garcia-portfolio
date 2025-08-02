a5114a43add9ec8a2b469947a6cbd7b745ec4ec3
Olga Pinheiro <olga.pinheiro@jusbrasil.com.br>
2025-08-02 12:44:11 -0300

    feat: improve image modal layout

--- src/components/ImageModal.tsx
+++ src/components/ImageModal.tsx
@@ -17,14 +17,14 @@ const ImageModal = ({
     <div>
       {modalOpen &&
         <div
-          className='z-10 fixed top-0 left-0 w-full h-full bg-white/98 dark:bg-black/98 flex flex-col justify-between items-center p-8'
+          className='z-10 fixed top-0 left-0 w-full h-full bg-white/98 dark:bg-black/98 flex flex-col justify-center items-center p-0'
           role="dialog"
           aria-modal="true"
           tabIndex={0}
           onClick={handleModal}
           onKeyDown={(e) => { if (e.key.toLowerCase() === 'escape') handleModal() }}
         >
-          <div className="w-full flex flex-row justify-end">
+          <div className="w-full flex flex-row justify-end p-4">
           <button
             type='button'
             onClick={handleModal}
