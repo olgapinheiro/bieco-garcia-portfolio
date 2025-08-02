26c06af1da36762bc2678fa4c3b23cb046c08ba9
Olga Pinheiro <olga.pinheiro@jusbrasil.com.br>
2025-08-02 02:25:21 -0300

    fix: centralize Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ interface FooterProps {
 
 export default function Footer({ className = "" }: FooterProps) {
   return (
-    <footer className={`container bg-white dark:bg-black flex flex-col md:flex-row gap-4 items-center justify-between py-4 px-10 ${className}`}>
+    <footer className={`w-full bg-white dark:bg-black flex flex-col gap-4 items-center justify-center py-4 px-10 ${className}`}>
       <SocialMediaIcons />
       <div className="flex gap-6 flex-wrap items-center justify-center text-xs">
         © 2025 BIECO GARCIA
