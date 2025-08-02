616b439058d07605bdf1b42b03418436194b8eb8
Olga Pinheiro <olga.pinheiro@jusbrasil.com.br>
2025-08-02 01:46:22 -0300

    feat: update `Header` with social media and nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,23 @@
 import Link from "next/link";
+import NavDesktop from "./NavDesktop";
+import NavMobile from "./NavMobile";
 
 export default function Header() {
   return (
-    <header className="outline bg-white dark:bg-black flex items-center justify-center py-4">
-      <Link
-        className="flex items-center gap-2 font-bold text-4xl"
-        href="/portfolio"
-      >
-        BIECO GARCIA
-      </Link>
+    <header className="w-full outline bg-white dark:bg-black flex flex-row gap-4 items-center justify-center p-3">
+      <div className="container flex flex-row gap-4 items-center justify-between">
+        <div className="flex gap-6 flex-wrap items-center justify-center">
+          <Link
+            className="flex items-center gap-2 text-xl hover:underline underline-offset-4"
+            href="/"
+          >
+            BIECO GARCIA
+          </Link>
+        </div>
+
+        <NavDesktop />
+        <NavMobile />
+      </div>
     </header>
   )
 }
