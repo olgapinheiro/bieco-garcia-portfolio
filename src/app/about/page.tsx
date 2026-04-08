3892ad212b05386fbefe5aa53a2f5de5e9c89157
Olga Pinheiro <olgapinheirogarcia@gmail.com>
2026-04-08 10:16:07 -0300

    feat: add new logo images and update links in About page carousel
    - Added multiple new logo images for various brands to the About page.
    - Updated logo links to direct users to the respective brand websites.
    - Ensured all logos are displayed with consistent dimensions for improved layout.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,130 +11,130 @@ const LOGO_IMAGES = [
     link: 'https://westin.marriott.com/',
   },
   {
-    src: 'brands/westin_logo.svg',
+    src: '/brands/ibis_logo.png',
     alt: 'Ibis',
     width: 60,
     height: 60,
-    link: '',
+    link: 'https://ibis.accor.com',
   },
   {
-    src: 'brands/westin_logo.svg',
-    alt: 'Marriot',
+    src: '/brands/marriott_logo.png',
+    alt: 'Marriott',
     width: 60,
     height: 60,
-    link: '',
+    link: 'https://www.marriott.com/',
   },
   {
-    src: 'brands/westin_logo.svg',
+    src: '/brands/accor_logo.png',
     alt: 'Accor',
     width: 60,
     height: 60,
-    link: '',
+    link: 'https://all.accor.com/',
   },
   {
-    src: 'brands/westin_logo.svg',
+    src: '/brands/ESH_logo.png',
     alt: 'ESH',
     width: 60,
     height: 60,
-    link: '',
+    link: 'https://eshhotel.com/',
   },
   {
-    src: 'brands/westin_logo.svg',
+    src: '/brands/cicchetti_logo.png',
     alt: 'CICCHETTI',
     width: 60,
     height: 60,
-    link: '',
+    link: 'https://bio.site/cicchetti',
   },
   {
-    src: 'brands/westin_logo.svg',
-    alt: 'Hotel Boutique Expérience',
+    src: '/brands/hotel-boutique_logo.png',
+    alt: 'Pedras do Patacho - Hotel Boutique Experience',
     width: 60,
     height: 60,
-    link: '',
+    link: 'https://pedrasdopatacho.com.br/',
   },
   {
-    src: 'brands/westin_logo.svg',
+    src: '/brands/armacao_logo.png',
     alt: 'Armação Resort',
     width: 60,
     height: 60,
-    link: '',
+    link: 'https://www.armacaoresort.com/',
   },
   {
-    src: 'brands/westin_logo.svg',
+    src: '/brands/kembali_logo.png',
     alt: 'Kembali',
     width: 60,
     height: 60,
-    link: '',
+    link: 'https://www.kembalihotel.com/',
   },
   {
-    src: 'brands/westin_logo.svg',
+    src: '/brands/filho-do-vento_logo.png',
     alt: 'Filho do Vento',
     width: 60,
     height: 60,
-    link: '',
+    link: 'https://filhodovento.com.br/',
   },
   {
-    src: 'brands/westin_logo.svg',
-    alt: 'Golden Pineapple Village',
+    src: '/brands/golden-pineapple_logo.png',
+    alt: 'Golden Pineapple Villas',
     width: 60,
     height: 60,
-    link: '',
+    link: 'https://www.simplebooking.it/ibe2/hotel/6479',
   },
   {
-    src: 'brands/westin_logo.svg',
+    src: '/brands/filha-da-lua_logo.png',
     alt: 'Filha da Lua Eco Lodge',
     width: 60,
     height: 60,
-    link: '',
+    link: 'https://filhadalua.com/',
   },
   {
-    src: 'brands/westin_logo.svg',
+    src: '/brands/ocapora_logo.png',
     alt: 'Ocaporã',
     width: 60,
     height: 60,
-    link: '',
+    link: 'https://www.ocapora.com.br/',
   },
   {
-    src: 'brands/westin_logo.svg',
+    src: '/brands/refugio-rio-bonito_logo.png',
     alt: 'Refúgio do Rio Bonito',
     width: 60,
     height: 60,
-    link: '',
+    link: 'https://www.refugiodoriobonito.com.br/',
   },
   {
-    src: 'brands/westin_logo.svg',
+    src: '/brands/tabaobi_logo.png',
     alt: 'Tabaobi',
     width: 60,
     height: 60,
-    link: '',
+    link: 'https://www.tabaobi.com.br/',
   },
   {
-    src: 'brands/westin_logo.svg',
+    src: '/brands/BYD_logo.png',
     alt: 'BYD',
     width: 60,
     height: 60,
-    link: '',
+    link: 'https://www.byd.com/',
   },
   {
-    src: 'brands/westin_logo.svg',
+    src: '/brands/kuat_logo.png',
     alt: 'Kuat',
     width: 60,
     height: 60,
-    link: '',
+    link: 'https://www.coca-cola.com/br/pt/brands/kuat',
   },
   {
-    src: 'brands/westin_logo.svg',
+    src: '/brands/ck_logo.png',
     alt: 'Calvin Klein',
     width: 60,
     height: 60,
-    link: '',
+    link: 'https://www.calvinklein.com',
   },
   {
-    src: 'brands/westin_logo.svg',
+    src: '/brands/beefeater_logo.png',
     alt: 'BEEFEATER',
     width: 60,
     height: 60,
-    link: '',
+    link: 'https://www.beefeatergin.com/',
   },
 ]
 
@@ -156,6 +156,7 @@ export default function About() {
             style={{
               objectFit: 'contain',
             }}
+            loading="eager"
           />
         </div>
         <div className="leading-6">
@@ -186,7 +187,7 @@ export default function About() {
       <div className="mt-12 max-w-4xl">
         <InfiniteSlider gap={24} speed={60} speedOnHover={40}>
           {LOGO_IMAGES.map((logo) => (
-            <a href={logo.link} target="_blank" rel="noopener noreferrer" key={logo.alt}>
+            <a href={logo.link || ''} target="_blank" rel="noopener noreferrer" key={logo.alt}>
               <Image
                 width={logo.width}
                 height={logo.height}
