912917126b6808354d5d7900ad1e5e358891e81d
Olga Pinheiro <olgapinheirogarcia@gmail.com>
2026-04-11 15:20:17 -0300

    refactor: improve Header component layout and styling
    - Adjusted padding in the Header component for better spacing.
    - Updated the container class to enhance responsiveness and layout consistency.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,11 +24,11 @@ export default function Header() {
 
   return (
     <header
-      className={`fixed top-0 left-0 right-0 z-50 w-full bg-white dark:bg-black flex flex-row gap-4 items-center justify-center p-3 transition-opacity duration-300 ${
+      className={`fixed top-0 left-0 right-0 z-50 w-full bg-white dark:bg-black flex flex-row gap-4 items-center justify-center py-3 transition-opacity duration-300 ${
         visible ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"
       }`}
     >
-      <div className="container flex flex-row gap-4 items-center justify-between">
+      <div className="w-full px-8 xs:px-16 flex flex-row items-center justify-between">
         <div className="flex gap-6 flex-wrap items-center justify-center">
           <Link
             className="flex items-center gap-2 text-lg hover:opacity-70 transition-opacity"
